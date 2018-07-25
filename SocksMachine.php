bc1460607d9073617ed0ff913af49b8c389af6fa
pekhotin <pekhotin@saject.ru>
2018-07-25 22:55:42 +0500

    Первая задачка: фикс ошибки неопределенного значения

--- SocksMachine.php
+++ SocksMachine.php
@@ -3,7 +3,7 @@
 class SocksMachine
 {
     public static function countUnpaired(array $socks) {
-        $cntColors = array();
+        $cntColors = array_fill(0, 1001, 0);
         foreach ($socks as $sock) {
             $cntColors[$sock]++;
         }
