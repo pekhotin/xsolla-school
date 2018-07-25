bd5d5cf8e1cc0140f7c43d594ed2076311287a1a
pekhotin <pekhotin@saject.ru>
2018-07-25 23:33:48 +0500

    Первая задачка: Добавлены комментарии.

--- SocksMachine.php
+++ SocksMachine.php
@@ -2,14 +2,16 @@
 
 class SocksMachine
 {
+    //Метод решил сделать статичным
     public static function countUnpaired(array $socks) {
-        $cntColors = array_fill(0, 1001, 0);
+        $cntColors = array_fill(0, 1001, 0); // Массив, i-й элемент которого показывает, сколько имеется носков цвета i.
         foreach ($socks as $sock) {
             $cntColors[$sock]++;
         }
 
         $result = 0;
 
+        //Считаем, сколько получилось нечетных цветов.
         foreach ($cntColors as $color) {
             if ($color % 2)
                 $result++;
@@ -18,5 +20,6 @@ class SocksMachine
         return $result;
     }
 }
-$testSocks = [10, 30, 10, 30, 10, 1, 2, 3, 4, 6];
-echo SocksMachine::countUnpaired($testSocks);
+//Проверка.
+//$testSocks = [10, 30, 10, 30, 10, 1, 2, 3, 4, 6];
+//echo SocksMachine::countUnpaired($testSocks);
