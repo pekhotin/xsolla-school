0ded7ebd06135dc6790d25596986d9c9ddb2a033
pekhotin <pekhotin@saject.ru>
2018-07-25 22:48:10 +0500

    Первая задачка

---
+++ SocksMachine.php
@@ -0,0 +1,22 @@
+<?php
+
+class SocksMachine
+{
+    public static function countUnpaired(array $socks) {
+        $cntColors = array();
+        foreach ($socks as $sock) {
+            $cntColors[$sock]++;
+        }
+
+        $result = 0;
+
+        foreach ($cntColors as $color) {
+            if ($color % 2)
+                $result++;
+        }
+
+        return $result;
+    }
+}
+$testSocks = [10, 30, 10, 30, 10, 1, 2, 3, 4, 6];
+echo SocksMachine::countUnpaired($testSocks);
