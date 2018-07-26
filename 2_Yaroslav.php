1f22cbd807b9084c93458e8fc8ba25b740ea59ab
pekhotin <pekhotin@saject.ru>
2018-07-26 15:15:55 +0500

    Вторая задачка: Вывод ответа

--- 2_Yaroslav.php
+++ 2_Yaroslav.php
@@ -4,10 +4,15 @@ $inputJson = file_get_contents('php://input');
 $obj = json_decode($inputJson, true);
 
 if (!isset($obj['cells']) || !isset($obj['distance'])) {
-    exit("Данные не получены");
+    exit("Data not received");
 }
 
 $cells = $obj['cells'];
 $distance = $obj['distance'];
+$result = 0;
 
-print_r($cells);
+
+$r["result"] = $result;
+$resultString = json_encode($r);
+file_put_contents('php://output', $resultString);
+// echo '{"result": ' . $result . '}';
